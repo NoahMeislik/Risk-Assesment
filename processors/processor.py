bf97894dc3b7d0d314f4d84a3b0f99c93c3dc3da
Noah Meislik <noah.meislik@gmail.com>
2018-05-06 20:48:26 -0700

    Added data split
    custom train, dev, test

--- processors/processor.py
+++ processors/processor.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import sklearn
+import math
 
 class Processor():
     """
@@ -14,4 +15,40 @@ class Processor():
     """
     def __init__(self, dev_percent, test_percent, batch_type, batch_size):
         
+        self.dev_percent = dev_percent
+        self.test_percent = test_percent
+        self.batch_type = batch_type
+        self.batch_size = batch_size
+
+    def split_data(self, X, Y):
+        """
+        Splits data into three parts: train, dev, test
+
+        Required Args:
+            X: Array of X data
+            Y: Array of Y data
+
+        Output:
+            train_x
+            train_y
+            dev_x
+            dev_y
+            test_x
+            text_y
+        """
+
+
+        dev_samples = math.floor((self.dev_percent / 100) * len(Y))
+        test_samples = math.floor((self.test_percent / 100) * len(Y))
+        train_samples = len(Y) - (dev_samples + test_samples)
+
+        train_set_x = X[:train_samples, :]
+        train_set_y = Y[:train_samples]
+        dev_set_x = X[train_samples:dev_samples + train_samples, :]
+        dev_set_y = Y[train_samples:dev_samples + train_samples]
+        test_set_x = X[dev_samples + train_samples:, :]
+        test_set_y = Y[dev_samples + train_samples:]
+
+        return train_set_x, train_set_y, dev_set_x, dev_set_y, test_set_x, test_set_y
+
 
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from processors.preprocessor import Imputer, Normalizer
+from processors.processor import Processor
 import os
 import pandas as pd
 import numpy as np
@@ -26,8 +27,10 @@ X, Y = load_dataset(year=1, shuffle=True)
 
 imputer = Imputer(strategy='mean')
 normalizer = Normalizer(strategy="l2", norm_axis=1)
+processor = Processor(10, 10, "regularize", 64)
 
 X = imputer.fit_transform(abstracts=X)
 X = normalizer.transform(abstracts=X)
 
-print(X.shape)
+x_train, y_train, x_dev, y_dev, x_test, y_test = processor.split_data(X, Y)
+print(x_train.shape, y_train.shape, x_dev.shape, y_dev.shape, x_test.shape, y_test.shape)
